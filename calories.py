c96a85df6d353ea3e82c86313b0c33054e673b84
eyebullsjohnson <bullseyejohnson@gmail.com>
2024-05-28 16:14:35 +0000

    calculates amount of calories comsumed in 1 week

---
+++ calories.py
@@ -0,0 +1,10 @@
+daily_calories = 1600
+days = 7
+
+calories = daily_calories * days
+
+display_message = "You have " + str(calories) + " calories this week"
+
+print(display_message)
+
+print("have a" + " day.")
